c4154cdb1cb83432fb1ac10e027f12d82becd387
Megaclick <axel.silva.14@sansano.usm.cl>
2018-12-04 20:49:16 -0300

    tarea_7 terminada (creo)

---
+++ app/src/main/java/cl/telematica/basicconnectionexample/main/GetData.java
@@ -0,0 +1,12 @@
+package cl.telematica.basicconnectionexample.main;
+import java.util.List;
+
+import cl.telematica.basicconnectionexample.models.Libro;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetData {
+
+    @GET("/v2/5bfc6aa9310000780039be36")
+    Call<List<Libro>> getAllData();
+}
--- app/src/main/java/cl/telematica/basicconnectionexample/main/MainActivity.java
+++ app/src/main/java/cl/telematica/basicconnectionexample/main/MainActivity.java
@@ -1,22 +1,29 @@
 package cl.telematica.basicconnectionexample.main;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import cl.telematica.basicconnectionexample.R;
+import cl.telematica.basicconnectionexample.connection.RetrofitClientInstance;
 import cl.telematica.basicconnectionexample.main.presenter.MainPresenterImpl;
 import cl.telematica.basicconnectionexample.models.Libro;
 import cl.telematica.basicconnectionexample.main.view.MainView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements MainView {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,29 @@ public class MainActivity extends AppCompatActivity implements MainView {
 
         setRecyclerViewParams();
 
-        MainPresenterImpl presenter = new MainPresenterImpl(this);
-        presenter.fetchData("http://www.mocky.io/v2/5bfc6aa9310000780039be36", 15000);
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading . . . . .");
+        progressDialog.show();
+
+        //MainPresenterImpl presenter = new MainPresenterImpl(this);
+        //presenter.fetchData("http://www.mocky.io/v2/5bfc6aa9310000780039be36", 15000);
+
+        GetData service = RetrofitClientInstance.getRetrofitInstance().create(GetData.class);
+        Call<List<Libro>> call = service.getAllData();
+        call.enqueue(new Callback<List<Libro>>() {
+            @Override
+            public void onResponse(Call<List<Libro>> call, Response<List<Libro>> response) {
+                progressDialog.dismiss();
+                populateRecyclerView(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Libro>> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "AYURAAAAAA", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
